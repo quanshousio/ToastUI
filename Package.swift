f1d2840ce7f0e5070f8c94e6eeac60466967bef8
Quan Tran <quan@shousio.com>
2022-04-10 20:52:37 -0700

    Bump Swift version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
   name: "ToastUI",
-  platforms: [.iOS(.v14), .tvOS(.v14), .macOS(.v11)],
+  platforms: [
+    .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .macOS(.v11)
+  ],
   products: [
-    .library(
-      name: "ToastUI",
-      targets: ["ToastUI"]
-    )
+    .library(name: "ToastUI", targets: ["ToastUI"])
   ],
   targets: [
-    .target(
-      name: "ToastUI"
-    )
+    .target(name: "ToastUI")
   ]
 )
